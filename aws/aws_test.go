7a7cc317f3bdbe8366606c0df7e013ac42d97eab
Daisuke Maki <lestrrat+github@gmail.com>
2022-01-21 16:39:27 +0900

    Update example

--- aws/aws_test.go
+++ aws/aws_test.go
@@ -1,8 +1,10 @@
 package awssigner_test
 
 import (
+	"bytes"
 	"context"
 	"crypto"
+	"os"
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -16,15 +18,19 @@ import (
 var _ crypto.Signer = &awssigner.RSA{}
 
 func Example() {
+	// Make sure to set AWS_* environment variable, if you
+	// need to configure them.
 	awscfg, err := config.LoadDefaultConfig(
 		context.Background(),
-		config.WithRegion("us-central-1"),
 	)
 	if err != nil {
 		panic(err.Error())
 	}
 
-	kid := "your AWS kid"
+	kid := os.Getenv(`AWS_KMS_KEY_ID`)
+	if kid == "" {
+		panic(`missing AWS_KMS_KEY_ID`)
+	}
 	payload := []byte("obla-di-obla-da")
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
@@ -33,11 +39,18 @@ func Example() {
 		WithAlgorithm(types.SigningAlgorithmSpecRsassaPkcs1V15Sha256).
 		WithKeyID(kid)
 
-	signed, err := jws.Sign(payload, jwa.ES256, sv.WithContext(ctx))
+	signed, err := jws.Sign(payload, jwa.RS256, sv.WithContext(ctx))
 	if err != nil {
 		panic(err.Error())
 	}
-	if _, err := jws.Verify(signed, jwa.ES256, sv.WithContext(ctx)); err != nil {
+
+	verified, err := jws.Verify(signed, jwa.RS256, sv.WithContext(ctx))
+	if err != nil {
 		panic(err.Error())
 	}
+
+	if bytes.Compare(payload, verified) != 0 {
+		panic("payload and verified does not match")
+	}
+	//OUTPUT:
 }
